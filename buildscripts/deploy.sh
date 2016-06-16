ededc5a35659077418c73eb427dc8a79137cffd4
Arnaud Weyts <arnaud.weyts@gmail.com>
2016-06-16 11:22:20 +0200

    added deploy script

---
+++ buildscripts/deploy.sh
@@ -0,0 +1,54 @@
+#!/bin/bash -e
+# deploy script for a jekyll blog
+# heavily based on: https://gist.github.com/domenic/ec8b0fc8ab45f39403dd
+
+SOURCE_BRANCH="master"
+TARGET_BRANCH="gh-pages"
+GH_REF="github.com/ArnaudWeyts/blog"
+
+function doCompile {
+  bundle exec jekyll build
+}
+
+# Pull requests and commits to other branches shouldn't try to deploy, just build to verify
+if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
+    echo "Skipping deploy; just doing a build."
+    doCompile
+    exit 0
+fi
+
+# Save some useful information
+REPO=`git config remote.origin.url`
+SHA=`git rev-parse --verify HEAD`
+
+# Clone the existing gh-pages for this repo into _site/
+# Create a new empty branch if gh-pages doesn't exist yet (should only happen on first deploy)
+git clone $REPO _site
+cd _site
+git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
+cd ..
+
+# Clean out existing contents
+# commented out because jekyll build cleans up the directory itself
+# rm -rf _site/**/* || exit 0
+
+# Run our compile script
+doCompile
+
+# Now let's go have some fun with the cloned repo
+cd out
+git config user.name "Block-Bot"
+git config user.email "bot@weyts.xyz"
+
+# If there are no changes to the compiled out (e.g. this is a README update) then just bail.
+if [ -z `git diff --exit-code` ]; then
+    echo "No changes to the output on this push; exiting."
+    exit 0
+fi
+
+# Commit the "changes", i.e. the new version.
+git add .
+git commit -m "Deployed to GitHub Pages using Travis-CI: ${SHA}"
+
+# Now that we're all set up, we can push with the token
+git push -f "https://${GH_TOKEN}@${GH_REF}" ${TARGET_BRANCH} > /dev/null 2>&1
